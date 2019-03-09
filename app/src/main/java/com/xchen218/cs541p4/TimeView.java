02eda9bd464f4f02e2dff1e3836056699aca88ae
xchen218 <xchen218@binghamton.edu>
2019-03-09 18:55:49 -0500

    time

---
+++ app/src/main/java/com/xchen218/cs541p4/TimeView.java
@@ -0,0 +1,62 @@
+package com.xchen218.cs541p4;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import android.os.Handler;
+
+import java.util.Calendar;
+
+
+public class TimeView extends LinearLayout {
+    public TimeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    public TimeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public TimeView(Context context) {
+        super(context);
+    }
+    private TextView tvTime;
+    @Override
+    protected void onFinishInflate(){
+        super.onFinishInflate();
+        tvTime = findViewById(R.id.tvTime);
+        tvTime.setText("Hello");
+        timeHandler.sendEmptyMessage(0);
+    }
+
+    @Override
+    protected void onVisibilityChanged(View changedView, int visibility) {
+        super.onVisibilityChanged(changedView, visibility);
+        if(visibility == View.VISIBLE){
+            timeHandler.sendEmptyMessage(0);
+        }else{
+            timeHandler.removeMessages(0);
+        }
+    }
+
+    private void refreshTime() {
+        Calendar c = Calendar.getInstance();
+        if (c.get(Calendar.SECOND) < 10) {
+            tvTime.setText(String.format("%d:%d:0%d", c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), c.get(Calendar.SECOND)));
+        }else{
+            tvTime.setText(String.format("%d:%d:%d", c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), c.get(Calendar.SECOND)));
+        }
+    }
+
+    public Handler timeHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            refreshTime();
+            if(getVisibility() == View.VISIBLE){
+                timeHandler.sendEmptyMessageDelayed(0, 1000);
+            }
+        }
+    };
+
+}
+

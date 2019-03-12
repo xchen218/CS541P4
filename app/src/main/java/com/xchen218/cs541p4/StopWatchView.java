8babc58236c1771b5944d5d6ed517d457823d757
xchen218 <xchen218@binghamton.edu>
2019-03-11 21:08:56 -0400

    sw

---
+++ app/src/main/java/com/xchen218/cs541p4/StopWatchView.java
@@ -0,0 +1,11 @@
+package com.xchen218.cs541p4;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class StopWatchView extends LinearLayout {
+    public StopWatchView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}

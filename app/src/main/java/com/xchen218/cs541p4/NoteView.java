5f2acbf0e42fdb8d37331f7b753877da9209c7cf
xchen218 <xchen218@binghamton.edu>
2019-03-14 20:57:54 -0400

    f

--- app/src/main/java/com/xchen218/cs541p4/NoteView.java
+++ app/src/main/java/com/xchen218/cs541p4/NoteView.java
@@ -1,11 +1,17 @@
 package com.xchen218.cs541p4;
 
 import android.content.Context;
+import android.os.Environment;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class NoteView extends LinearLayout {
     public NoteView(Context context, AttributeSet attrs) {
@@ -24,10 +30,35 @@ public class NoteView extends LinearLayout {
         btnSave.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-            //    tvNote.getText().toString().
+                try{
+                    String etName =  tvNote.getText().toString();
+                    if(!etName.trim().equals("")){
+                        File file =new File(Environment.getExternalStorageDirectory().getPath(), "output.txt");
+
+                        //if file doesnt exists, then create it
+                        if(!file.exists()){
+                            file.createNewFile();
+                        }
+
+
+                        FileWriter fileWritter = new FileWriter(file.getName(),true);
+                        BufferedWriter bufferWritter = new BufferedWriter(fileWritter);
+                        bufferWritter.write(etName);
+                        bufferWritter.close();
+                    }
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+                tvNote.setText("");
             }
         });
         btnClear = findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvNote.setText("");
+            }
+        });
 
 
     }
